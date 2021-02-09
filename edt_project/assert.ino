ff683d0682f7a7f0b04d47dee2f893826b93445c
Joe <>
2021-02-09 09:23:15 +0000

    Add startup delay time to config file

--- edt_project/assert.ino
+++ edt_project/assert.ino
@@ -3,13 +3,23 @@
 
 void assert(bool condition, size_t errorCode)
 {
+
 #if defined(DEBUG)
+
+	// if in debug mode, then check for the error
+
 	if (!condition)
 	{
 		throwError(errorCode);
 	}
+
 #else
+
 	// should do nothing when not in debug mode
+	// and should get entirely optimised away by
+	// the -Os
+
 #endif
+
 }
 
--- edt_project/config.hpp
+++ edt_project/config.hpp
@@ -5,14 +5,27 @@
 	that should be corrected after testing.
 */
 
+// define if assertions should be used
+// (slow down drone program, but add error checking)
 #define DEBUG
 
+// main PID values
 #define KP 1
 #define KI 1
 #define KD 1
 
+// max and min values of the escs
+// NOTE: you may need to set the minimum
+// 	 higher than the default value to
+// 	 still get some thrust
 #define MAXMOTORVALUE 2000
 #define MINMOTORVALUE 1000
 
+// amount of times between each time an error should be
+// reported
 #define ERROR_REPEAT_DELAY 1000
+
+// amount of milliseconds taken for the drone to wait
+// before attempting to fly
+#define DRONE_STARTUP_DELAY 10000
 
