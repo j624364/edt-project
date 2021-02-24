93e4c74e1acf8056719c2ffef783841e13f91c5a
Joe <>
2021-02-24 14:45:08 +0000

    Move axis_data to new file

---
+++ edt_project/axis_data.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+struct axis_data
+{
+	float x = -1.0f;
+	float y = -1.0f;
+	float z = -1.0f;
+};
--- edt_project/gyroscope.hpp
+++ edt_project/gyroscope.hpp
@@ -2,12 +2,5 @@
 
 #include "platform.hpp"
 
-struct axis_data
-{
-	float x = 0.0f;
-	float y = 0.0f;
-	float z = 0.0f;
-};
-
 void SetupMPU();
 void ReadIMUValues(axis_data& gyroData);
