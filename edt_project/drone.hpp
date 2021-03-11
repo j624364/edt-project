0fd1ee85b77661e9fdb9dfdd48cc913536785cac
Joe <>
2021-03-11 11:11:10 +0000

    Add vscode typedef

--- edt_project/drone.hpp
+++ edt_project/drone.hpp
@@ -2,6 +2,12 @@
 
 // contains all of the function definitions
 
+// vscode does not recognise size_t so
+// im defining it here
+#if defined(VSCODE)
+using size_t = unsigned int;
+#endif
+
 #include "axis_data.hpp"
 #include "main.hpp"
 #include "motor.hpp"
